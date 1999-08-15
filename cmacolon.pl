3fce4b9e602e0a4697252215e49a32c6828f057d
richard <richard>
1999-08-15 21:50:25 +0000

    Make output be more useful for making changes to.

--- cmacolon.pl
+++ cmacolon.pl
@@ -13,12 +13,20 @@ while(<>) {
     $sma = substr($_,10,6);
     $eng = substr($_,20,40);
     $eng =~ s/([^ ])[ ]*$/$1/;
-    if ($cma =~ /^[aeiou]\'?[aeiou]/) {
-	print $cma.":{..".$eng."}\n";
-    } elsif ($sma =~ /^[ ]*UI/) {
-	print $cma.":{..".$eng."}\n";
-    } else {
-	print $cma.":".$eng."\n";
-    }
+    $mng = substr($_,62);
+    chop $mng;
+    $mng =~ s/([^ ])[ ]*$/$1/;
+
+    printf ("# %8s %8s %s\n", $cma, $sma, $mng);
+    print $cma.":".$eng.":ORIGINAL\n";
+    print "# ".$cma.":".$eng.":MODIFIED\n\n";
+
+#    if ($cma =~ /^[aeiou]\'?[aeiou]/) {
+#	print $cma.":{..".$eng."}\n";
+#    } elsif ($sma =~ /^[ ]*UI/) {
+#	print $cma.":{..".$eng."}\n";
+#    } else {
+#	print $cma.":".$eng."\n";
+#    }
 }
 
