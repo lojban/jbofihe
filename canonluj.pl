4c0ac423de873466927c998eac4c43aa89715470
richard <richard>
2000-05-01 21:04:55 +0000

    Use gismu table rather than rafsi table as the input (rafsi table seems to be out of date).

--- canonluj.pl
+++ canonluj.pl
@@ -1,17 +1,22 @@
 #!/usr/bin/env perl
 # $Header$
 #
-# Create insert for canonluj.c with the rafsi->gismu table.
+# Create insert for canonluj.c with the rafsi in the gismu table.
 #
 # COPYRIGHT
 
 %rafsi = ();
 while (<STDIN>) {
     chomp;
-    m/^([^ ]+) +([^ ]+) / || die "Unmatched [$_]";
-    my $r = $1;
-    my $g = $2;
-    $rafsi{$r} = $g;
+    $gismu = substr($_,0,5);
+    $gismu =~ s/ +$//;
+    $rafsi = substr($_,6,12);
+    $rafsi =~ s/ +/ /g;
+    $rafsi =~ s/^ //;
+    $rafsi =~ s/ $//;
+    foreach (split / /,$rafsi) {
+	$rafsi{$_} = $gismu;
+    }
     if (length $g == 5) {
 	$rafsi{substr($g,0,4)} = $g;
     }
