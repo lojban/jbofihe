4d0247b3b16ceda59c32211c85e545d676d1af8e
richard <richard>
2000-11-23 23:36:06 +0000

    Added handling for sa and su

--- erasure.c
+++ erasure.c
@@ -101,21 +101,105 @@ handle_si(TreeNode *toks)
 }
 
 /*++++++++++++++++++++++++++++++
+  Handle the 'sa' erasure cmavo.  The definition this is working to is to erase
+  back to something in the same selma'o as the word which follows.  If this
+  hits the start of the text, erase everything up to the sa.
   ++++++++++++++++++++++++++++++*/
 
 static void
 handle_sa(TreeNode *toks)
 {
+  TreeNode *x, *y, *nt;
 
+  for (x = toks->next;
+       x != toks;
+       x = nt) {
+
+    nt = x->next;
+
+    /*
+       If x is last token, no following token to determine the type of what to
+       erase back to.  In this case, the SA is ignored (which will cause a
+       parse error later.) The alternative is to treat it as erasing the entire
+       text, which seems heavy-handed.
+       */
+
+    if (nt == toks) break;
+
+    if ((x->type != N_CMAVO) ||
+        (x->data.cmavo.selmao != SA)) continue;
+
+    for (y = x->prev; y != toks; y = y->prev) {
+      if ((y->type == nt->type) &&
+          (((y->type == N_CMAVO) && (y->data.cmavo.selmao == nt->data.cmavo.selmao)) ||
+           (y->type != N_CMAVO))) {
+        break;
+      }      
+    }
+
+    if (y == toks) y = toks->next;
+
+    while (y != nt) {
+      TreeNode *ntt = y->next;
+      delete_node(y);
+      y = ntt;
+    }
+  }
 }
 
 /*++++++++++++++++++++++++++++++
+NIhO, LU, TUhE, and TO.
   ++++++++++++++++++++++++++++++*/
 
 static void
 handle_su(TreeNode *toks)
 {
+  TreeNode *x, *y, *nt;
 
+  for (x = toks->next;
+       x != toks;
+       x = nt) {
+
+    nt = x->next;
+
+    if ((x->type == N_CMAVO) &&
+        (x->data.cmavo.selmao == SU)) {
+
+      if ((nt != toks) &&
+          (nt->type == N_CMAVO) &&
+          (nt->data.cmavo.selmao == SU)) {
+        /* double SU, delete all text up to date */
+        
+        TreeNode *u, *nu, *ntt;
+        ntt = nt->next;
+        for (u = toks->next; u != ntt; u = nu) {
+          nu = u->next;
+          delete_node(u);
+        }
+        nt = ntt;
+        
+      } else {
+        /* single SU */
+        for (y = x->prev; y != toks; y = y->prev) {
+          if ((y->type == N_CMAVO) &&
+              ((y->data.cmavo.selmao == NIhO) ||
+               (y->data.cmavo.selmao == LU) ||
+               (y->data.cmavo.selmao == TUhE) ||
+               (y->data.cmavo.selmao == TO))) {
+            break;
+          }      
+        }
+
+        if (y == toks) y = toks->next;
+
+        while (y != nt) {
+          TreeNode *ntt = y->next;
+          delete_node(y);
+          y = ntt;
+        }
+      }
+    }
+  }
 }
 
   
