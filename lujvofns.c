968f7111e3077d94420731756954d38dc9f0e9cc
richard <richard>
2000-11-30 23:58:29 +0000

    Rewrote to use a char. attribute table rather than strchr

--- lujvofns.c
+++ lujvofns.c
@@ -11,6 +11,52 @@
 
 #include "lujvofns.h"
 
+#define CONSONANT 0001
+#define VOWEL     0002
+#define LETTER_Y  0004
+#define APOS      0010
+#define SIBILANT  0020
+#define VOICED    0040
+#define UNVOICED  0100
+#define UPPERCASE 0200
+
+unsigned char attr_table[256] = {
+  0000, 0000, 0000, 0000, 0000, 0000, 0000, 0000, /* 000 - 007 */
+  0000, 0000, 0000, 0000, 0000, 0000, 0000, 0000, /* 010 - 017 */
+  0000, 0000, 0000, 0000, 0000, 0000, 0000, 0000, /* 020 - 027 */
+  0000, 0000, 0000, 0000, 0000, 0000, 0000, 0000, /* 030 - 037 */
+  0000, 0000, 0000, 0000, 0000, 0000, 0000, 0010, /* 040 - 047 */
+  0000, 0000, 0000, 0000, 0000, 0000, 0000, 0000, /* 050 - 057 */
+  0000, 0000, 0000, 0000, 0000, 0000, 0000, 0000, /* 060 - 067 */
+  0000, 0000, 0000, 0000, 0000, 0000, 0000, 0000, /* 070 - 077 */
+
+  0000, 0202, 0241, 0321, 0241, 0202, 0301, 0241, /* 100 - 107 */
+  0200, 0202, 0261, 0301, 0201, 0201, 0201, 0202, /* 110 - 117 */
+  0301, 0200, 0201, 0321, 0301, 0202, 0241, 0200, /* 120 - 127 */
+  0301, 0204, 0261, 0000, 0000, 0000, 0000, 0000, /* 130 - 137 */
+  0000, 0002, 0041, 0121, 0041, 0002, 0101, 0041, /* 140 - 147 */
+  0000, 0002, 0061, 0101, 0001, 0001, 0001, 0002, /* 150 - 157 */
+  0101, 0000, 0001, 0121, 0101, 0002, 0041, 0000, /* 160 - 167 */
+  0101, 0004, 0061, 0000, 0000, 0000, 0000, 0000, /* 170 - 177 */
+
+  0000, 0000, 0000, 0000, 0000, 0000, 0000, 0000, /* 200 - 207 */
+  0000, 0000, 0000, 0000, 0000, 0000, 0000, 0000, /* 210 - 217 */
+  0000, 0000, 0000, 0000, 0000, 0000, 0000, 0000, /* 220 - 227 */
+  0000, 0000, 0000, 0000, 0000, 0000, 0000, 0000, /* 230 - 237 */
+  0000, 0000, 0000, 0000, 0000, 0000, 0000, 0000, /* 240 - 247 */
+  0000, 0000, 0000, 0000, 0000, 0000, 0000, 0000, /* 250 - 257 */
+  0000, 0000, 0000, 0000, 0000, 0000, 0000, 0000, /* 260 - 267 */
+  0000, 0000, 0000, 0000, 0000, 0000, 0000, 0000, /* 270 - 277 */
+
+  0000, 0000, 0000, 0000, 0000, 0000, 0000, 0000, /* 300 - 307 */
+  0000, 0000, 0000, 0000, 0000, 0000, 0000, 0000, /* 310 - 317 */
+  0000, 0000, 0000, 0000, 0000, 0000, 0000, 0000, /* 320 - 327 */
+  0000, 0000, 0000, 0000, 0000, 0000, 0000, 0000, /* 330 - 337 */
+  0000, 0000, 0000, 0000, 0000, 0000, 0000, 0000, /* 340 - 347 */
+  0000, 0000, 0000, 0000, 0000, 0000, 0000, 0000, /* 350 - 357 */
+  0000, 0000, 0000, 0000, 0000, 0000, 0000, 0000, /* 360 - 367 */
+  0000, 0000, 0000, 0000, 0000, 0000, 0000, 0000  /* 370 - 377 */
+};
 
 /*++++++++++++++++++++++++++++++++++++++
   
@@ -22,11 +68,7 @@
 
 int
 is_consonant(char c) {
-  if (strchr("bcdfgjklmnprstvxz", c)) {
-    return 1;
-  } else {
-    return 0;
-  }
+  return !!(attr_table[c] & CONSONANT);
 }
 
 /*++++++++++++++++++++++++++++++++++++++
@@ -39,11 +81,8 @@ is_consonant(char c) {
 
 int
 is_uppercase_consonant(char c) {
-  if (strchr("BCDFGJKLMNPRSTVXZ", c)) {
-    return 1;
-  } else {
-    return 0;
-  }
+  int m = CONSONANT | UPPERCASE;
+  return ((attr_table[c] & m) == m);
 }
 
 /*++++++++++++++++++++++++++++++++++++++
@@ -56,13 +95,8 @@ is_uppercase_consonant(char c) {
 
 int
 is_vowel(char c) {
-  if (strchr("aeiou", c)) {
-    return 1;
-  } else {
-    return 0;
-  }
+  return !!(attr_table[c] & VOWEL);
 }
-
 
 /*++++++++++++++++++++++++++++++++++++++
   
@@ -74,13 +108,8 @@ is_vowel(char c) {
 
 int
 is_voiced(char c) {
-  if (strchr("bdgvjz", c)) {
-    return 1;
-  } else {
-    return 0;
-  }
+  return !!(attr_table[c] & VOICED);
 }
-
 
 /*++++++++++++++++++++++++++++++++++++++
   
@@ -92,13 +121,8 @@ is_voiced(char c) {
 
 int
 is_unvoiced(char c) {
-  if (strchr("ptkfcsx", c)) {
-    return 1;
-  } else {
-    return 0;
-  }
+  return !!(attr_table[c] & UNVOICED);
 }
-
 
 /*++++++++++++++++++++++++++++++++++++++
   
@@ -110,13 +134,8 @@ is_unvoiced(char c) {
 
 int
 is_sibilant(char c) {
-  if (strchr("cjsz", c)) {
-    return 1;
-  } else {
-    return 0;
-  }
+  return !!(attr_table[c] & SIBILANT);
 }
-
 
 /*++++++++++++++++++++++++++++++++++++++
   
@@ -128,9 +147,9 @@ is_sibilant(char c) {
 
 int
 is_ccv(char *s) {
-  if (s[0] && is_consonant(s[0]) &&
-      s[1] && is_consonant(s[1]) &&
-      s[2] && is_vowel(s[2])) {
+  if (is_consonant(s[0]) &&
+      is_consonant(s[1]) &&
+      is_vowel(s[2])) {
     return 1;
   } else {
     return 0;
@@ -148,10 +167,10 @@ is_ccv(char *s) {
 
 int
 is_cvv(char *s) {
-  if (s[0] && is_consonant(s[0]) &&
-      s[1] && is_vowel(s[1]) &&
-      ((s[2] && is_vowel(s[2])) ||
-       (s[3] && is_vowel(s[3]) && s[2] == '\''))) {
+  if (is_consonant(s[0]) &&
+      is_vowel(s[1]) &&
+      (is_vowel(s[2]) ||
+       (is_vowel(s[3]) && s[2] == '\''))) {
     return 1;
   } else {
     return 0;
@@ -169,9 +188,9 @@ is_cvv(char *s) {
 
 int
 is_cvc(char *s) {
-  if (s[0] && is_consonant(s[0]) &&
-      s[1] && is_vowel(s[1]) &&
-      s[2] && is_consonant(s[2])) {
+  if (is_consonant(s[0]) &&
+      is_vowel(s[1]) &&
+      is_consonant(s[2])) {
     return 1;
   } else {
     return 0;
@@ -189,11 +208,11 @@ is_cvc(char *s) {
 
 int
 is_cvccv(char *s) {
-  if (s[0] && is_consonant(s[0]) &&
-      s[1] && is_vowel(s[1]) &&
-      s[2] && is_consonant(s[2]) &&
-      s[3] && is_consonant(s[3]) &&
-      s[4] && is_vowel(s[4])) {
+  if (is_consonant(s[0]) &&
+      is_vowel(s[1]) &&
+      is_consonant(s[2]) &&
+      is_consonant(s[3]) &&
+      is_vowel(s[4])) {
     return 1;
   } else {
     return 0;
@@ -211,11 +230,11 @@ is_cvccv(char *s) {
 
 int
 is_ccvcv(char *s) {
-  if (s[0] && is_consonant(s[0]) &&
-      s[1] && is_consonant(s[1]) &&
-      s[2] && is_vowel(s[2]) &&
-      s[3] && is_consonant(s[3]) &&
-      s[4] && is_vowel(s[4])) {
+  if (is_consonant(s[0]) &&
+      is_consonant(s[1]) &&
+      is_vowel(s[2]) &&
+      is_consonant(s[3]) &&
+      is_vowel(s[4])) {
     return 1;
   } else {
     return 0;
@@ -309,7 +328,7 @@ is_valid_lujvo(char *t) {
       if (is_consonant(t[0]) && is_consonant(t[1]) && is_vowel(t[2]) && is_consonant(t[3]) && is_initialpairok(t)) {
         if (debug) printf("4 letter rafsi CCVC at start valid then y\n");
         t+=5;
-      } else if (is_consonant(t[0]) && is_vowel(t[1]) && is_consonant(t[2]) && is_consonant(t[3]) && is_pairok(t)) {
+      } else if (is_consonant(t[0]) && is_vowel(t[1]) && is_consonant(t[2]) && is_consonant(t[3]) && is_pairok(t+2)) {
         if (debug) printf("4 letter rafsi CVCC at start valid then y\n");
         t+=5;
       } else {
@@ -384,4 +403,15 @@ is_valid_lujvo(char *t) {
   return 1;
 }
 
+
+#ifdef TEST
+int main() {
+  int c;
+  for (c='a'; c<='z'; c++) {
+    printf("%c c=%d uc=%d v=%d sib=%d voi=%d unv=%d\n",
+            c, is_consonant(c), is_uppercase_consonant(c), is_vowel(c), is_sibilant(c), is_voiced(c), is_unvoiced(c));
+  }
+  return 0;
+}
+#endif
 
