3b9d8a74ee1fa15581510790b9fa8ae366235d01
richard <richard>
2001-02-02 22:23:19 +0000

    Fix bugs for -se switch associated with boi/ve'o

--- elide.c
+++ elide.c
@@ -27,41 +27,66 @@
 extern int show_elisions;
 extern int show_elisions_verbose;
 
+/* Structure for holding 1 bit flags */
+
+typedef struct {
+  /* Is token is BOI or VEhO */
+  unsigned boiveho:1;
+
+  /* Flags indicating whether a rule of type 'subscript' was reduced *prior*
+     to this token being shifted */
+  unsigned subscript_before:1;
+
+  /* Flag indicating whether token is one of the private markers NUMBER_ROI,
+     NUMBER_MOI, NUMBER_MEI */
+  unsigned tok_is_number_marker:1;
+
+} OneBitFlags;
+
+
 /* Structure + single instance for buffering a sequence of zero or more
    potentially elidable separators/terminators followed by a single
    non-elidable symbol. */
 
 struct {
   int n; /* Number of tokens buffered */
-  int max; /* Current size of buf and flags arrays */
+  int max; /* Current size of dynamic arrays */
   int rp; /* Index of next token to be read */
   int sp; /* Index of next token to be shifted */
+
+  
   TokenType *buf; /* Token buffer */
 
-  /* flags array works as follows ...
+  /* conflicts array works as follows ...
   
-     flags[k] has (k-1) bits in it.  If the jth bit is set, it means the kth
+     conflicts[k] has (k-1) bits in it.  If the jth bit is set, it means the kth
      entry of buf could have been shifted in one of the reducing states that is
      encountered prior to the jth entry of buf being shifted. */
 
-  unsigned long **flags;
-  
+  unsigned long **conflicts;
+
+  OneBitFlags *flags;
+
 } tokbuf = { 
-  0, 0, 0, 0, NULL, NULL
+  0, 0, 0, 0, NULL, NULL, NULL
 };
+
+/* Flag indicating that previous token was PA prior to tokens being read into the tokbuf. */ 
+static int prev_token_was_pa_etc = 0;
+static int cur_token_was_pa_etc = 0;
 
 /*++++++++++++++++++++++++++++++++++++++
   Called to set a flag bit
   ++++++++++++++++++++++++++++++++++++++*/
 
 static inline void
-set_flag(int could_be_shifted, int lookahead)
+set_conflict(int could_be_shifted, int lookahead)
 {
   int j = lookahead;
   int k = could_be_shifted;
   int bank = j >> 5;
   unsigned long mask = 1UL << (j & 31);
-  tokbuf.flags[k][bank] |= mask;
+  tokbuf.conflicts[k][bank] |= mask;
 }
 
 /*++++++++++++++++++++++++++++++++++++++
@@ -75,7 +100,7 @@ is_set(int could_be_shifted, int lookahead)
   int k = could_be_shifted;
   int bank = j >> 5;
   unsigned long mask = 1UL << (j & 31);
-  return !!(tokbuf.flags[k][bank] & mask);
+  return !!(tokbuf.conflicts[k][bank] & mask);
 }
 
 /*++++++++++++++++++++++++++++++++++++++
@@ -99,13 +124,13 @@ can_shift_in_state(int state, int value)
   ++++++++++++++++++++++++++++++++++++++*/
 
 static void
-clear_flags(void)
+clear_conflicts(void)
 {
   int k, j, bank;
   unsigned long mask;
   for (k = 0; k < tokbuf.n; k++) {
     for (j = 0; j <= (k>>5); j++) {
-      tokbuf.flags[k][j] = 0UL;
+      tokbuf.conflicts[k][j] = 0UL;
     }
   }
 }
@@ -123,14 +148,12 @@ grow_buffer(void)
   first = tokbuf.max;
   tokbuf.max += 32;
   nblocks = tokbuf.max >> 5;
-  tokbuf.buf = (TokenType *) (tokbuf.buf ? Realloc(tokbuf.buf, sizeof(TokenType) * tokbuf.max) :
-                                           Malloc (            sizeof(TokenType) * tokbuf.max));
-                                          
-  tokbuf.flags = (unsigned long **) (tokbuf.flags ? Realloc(tokbuf.flags, sizeof(unsigned long *) * tokbuf.max) :
-                                                    Malloc (              sizeof(unsigned long *) * tokbuf.max));
+  tokbuf.buf       = grow_array(TokenType,       tokbuf.max, tokbuf.buf);
+  tokbuf.conflicts = grow_array(unsigned long *, tokbuf.max, tokbuf.conflicts);
+  tokbuf.flags     = grow_array(OneBitFlags,     tokbuf.max, tokbuf.flags);
 
   for (i=first; i < tokbuf.max; i++) {
-    tokbuf.flags[i] = (unsigned long *) Malloc(sizeof(unsigned long) * nblocks);
+    tokbuf.conflicts[i] = (unsigned long *) Malloc(sizeof(unsigned long) * nblocks);
   }
 }
 
@@ -139,16 +162,16 @@ grow_buffer(void)
   ++++++++++++++++++++++++++++++++++++++*/
 
 static int
-is_elidable(YYSTYPE tok)
+token_is_elidable(YYSTYPE tok, int *is_boiveho)
 {
   if (!tok) return 0; /* end of string */
   if (tok->type != N_CMAVO) return 0;
+
+  *is_boiveho = 0;
   
   switch (cmavo_table[tok->data.cmavo.code].code) {
     case CM_BEhO:
-    case CM_BOI:
     case CM_CU:
-    case CM_DOhU:
     case CM_FEhU:
     case CM_GEhU:
     case CM_KEI:
@@ -161,26 +184,92 @@ is_elidable(YYSTYPE tok)
     case CM_LUhU:
     case CM_MEhU:
     case CM_NUhU:
-    case CM_SEhU:
     case CM_TEhU:
-    case CM_TOI:
     case CM_TUhU:
     case CM_VAU:
-    case CM_VEhO:
       /* Any of these is elidable, with the exception that if the cmavo has
-         indicators attached to it, the meaning would obviously change if it
-         were omitted; in this case, it functions as a 'peg' on which to hang
-         indicators that apply to the entire construction it terminates. */
+         indicators or 'free' terms attached to it, the meaning would obviously
+         change if it were omitted; in this case, it functions as a 'peg' on
+         which to hang indicators/'free' terms that apply to the entire
+         construction it terminates. */
+
+      if (tok->data.cmavo.followed_by_free == 1) return 0;
+
       if (tok->ui_next != (TreeNode *)&tok->ui_next) {
         return 0;
       } else {
         return 1;
       }
       break;
+
+    /* These 3 can *never* have 'free' terms bound to them, because they only
+       ever terminate sub-rules of 'free' themselves.  Hence it is only
+       relevant whether they are followed by indicators. */
+    case CM_DOhU:
+    case CM_SEhU:
+    case CM_TOI:
+      if (tok->ui_next != (TreeNode *)&tok->ui_next) {
+        return 0;
+      } else {
+        return 1;
+      }
+      break;
+
+    case CM_BOI:
+    case CM_VEhO:
+
+      *is_boiveho = 1;
+    
+      /* These two have to be handled specially.  Because of the way they
+         appear in the 'subscript' rules, the appearance of a 'free' term
+         afterwards is not enough to force them to be included.  They are
+         unlike SEhU, DOhU, TOI because those can *never* have a 'free' term
+         attached to them.  BOI and VEhO can (within 'quantifier').  Hence, BOI
+         and VEhO have to be speculatively kept elidable, and a check made
+         later on as to whether one of the subscript rules is reduced after
+         they have been shifted. */  
+      if (tok->ui_next != (TreeNode *)&tok->ui_next) {
+        return 0;
+      } else {
+        return 1;
+      }
+      break;
+
     default:
       return 0;
       break;
   }
+}
+
+/*++++++++++++++++++++++++++++++++++++++
+  Detect whether a token is one of the private marker tokens that categ.c
+  inserts to ease the parser's task
+  ++++++++++++++++++++++++++++++++++++++*/
+
+static int
+token_is_number_marker(TreeNode *x)
+{
+  int result;
+  
+  if (x) {
+    if (x->type == N_MARKER) {
+      switch (x->data.marker.tok) {
+        case PRIVATE_NUMBER_ROI:
+        case PRIVATE_NUMBER_MAI:
+        case PRIVATE_NUMBER_MOI:
+          result = 1;
+          break;
+        default:
+          result = 0;
+          break;
+      }
+    } else {
+      result = 0;
+    }
+  } else {
+    result = 0;
+  }
+  return result;
 }
 
 /*++++++++++++++++++++++++++++++++++++++
@@ -308,7 +397,13 @@ produce_verbose_report(void)
     for (j=0; j<=i; j++) {
       if (j == i) {
         TreeNode *tok = tokbuf.buf[j].yylval;
-        printf(" <- %s\n", (j < tokbuf.n - 1) ? cmavo_table[tok->data.cmavo.code].cmavo : "<NEXT WORD>");
+        char *text;
+        if (j == (tokbuf.n - 1)) {
+          text = "<NEXT WORD>";
+        } else {
+          text = cmavo_table[tok->data.cmavo.code].cmavo;
+        }
+        printf(" <- %s\n", text);
       } else {
         printf("%s|", is_set(i, j) ? "X" : " ");
       }
@@ -337,6 +432,64 @@ elide_trace_shift(int yystate, int yychar)
     /* do reporting - the non-elidable has been shifted */
     int i, j, any_set=0;
     TreeNode *tok;
+    int nn;
+
+    /* Fix-up code for BOI & VEhO.  If one of these is followed by something
+       that starts a 'free' term, it is not until we know whether the BOI/VEhO
+       was reduced at the end of a subscript (rather than a quantifier) that we
+       can determine whether the BOI/VEhO has to be treated as non-elidable or
+       not */
+
+    nn = tokbuf.n; /* less typing */
+
+#if 0
+    printf("nn=%d\n", nn); fflush(stdout);
+    for (i=0; i<nn; i++) {
+      TreeNode *xx = tokbuf.buf[i].yylval;
+      printf("bv[%d]=%d sb[%d]=%d fbf[%d]=%d\n",
+             i, tokbuf.boiveho[i],
+             i, tokbuf.subscript_before[i],
+             i, xx ? xx->data.cmavo.followed_by_free : 0);
+      fflush(stdout);
+    }
+    printf("\n");
+#endif
+    
+    if (tokbuf.flags[nn-2].boiveho &&
+        !tokbuf.flags[nn-1].subscript_before &&
+        tokbuf.buf[nn-2].yylval->data.cmavo.followed_by_free) {
+      /* BOI/VEhO occurred at the end of a quantifier; if it was at the end
+         of a subscript, the parser would have reduced the subscript before
+         getting to the point where the 'free'-starting token could shift.
+         Therefore, the following 'free' term is attached to the BOI/VEhO
+         itself rather than being a peer of the subscript the BOI/VEhO is the
+         end of, so back up to make BOI/VEhO the non-elidable 'NEXT TOKEN' in
+         the report. */
+#if 0
+      printf("Retreated one token for boi/ve'o\n");
+#endif
+      tokbuf.n--;
+    }
+
+#if 0
+    printf("bv0=%d ptp=%d tnm[n-1]=%d\n",
+           tokbuf.flags[0].boiveho,
+           prev_token_was_pa_etc,
+           tokbuf.flags[tokbuf.n - 1].tok_is_number_marker);
+#endif
+
+    /* If non-elidable was a marker that precedes numbers, see whether first
+       elidable in the buffer is boi or ve'o, and whether this was preceded by
+       a token that would merge with the number if all the intervening
+       elidables were discarded.  If so, insert a synthetic conflict by
+       pretending that the marker is shiftable prior to the boi/ve'o.  This
+       will provide the required reporting later. */
+    if (tokbuf.flags[0].boiveho &&
+        prev_token_was_pa_etc &&
+        tokbuf.flags[tokbuf.n - 1].tok_is_number_marker) {
+        
+     set_conflict(tokbuf.n - 1, 0);
+    }
 
     if (show_elisions_verbose && (tokbuf.n > 1)) {
       printf("Early shift hazards for words ");
@@ -385,15 +538,15 @@ elide_trace_shift(int yystate, int yychar)
         /* Because it's a separator rather than a terminator, CU needs
            special treatment, because there will be a state where it conflicts
            with the following token for shifting. */
-#if TRACE_SR
-        int t1, t2, t3;
-        t1 = (i == tokbuf.n - 1);
-        t2 = (tokbuf.sp == tokbuf.n - 1);
+        int offset, priv, t1, t2, t3;
+        offset =  1;
+        t1 = (i == tokbuf.n - offset);
+        t2 = (tokbuf.sp == tokbuf.n - offset);
         t3 = (tokbuf.buf[tokbuf.sp - 1].value == CU);
+#if TRACE_SR
         printf("Got here 1 i=%d rp=%d sp=%d n=%d tests=%d,%d,%d\n", i, tokbuf.rp, tokbuf.sp, tokbuf.n, t1, t2, t3);
 #endif
-        if ((i == tokbuf.n - 1) && (tokbuf.sp == tokbuf.n - 1) &&
-            (tokbuf.buf[tokbuf.sp - 1].value == CU)) {
+        if (t1 && t2 && t3) {
           /* Do nothing */
         } else {
           /* Default case, set hazard flag.
@@ -402,7 +555,7 @@ elide_trace_shift(int yystate, int yychar)
 #if TRACE_SR
         printf("Got here 2, setting [%d,%d] in state %d\n", i, tokbuf.sp-1, yystate);
 #endif
-          set_flag(i, tokbuf.sp-1);
+          set_conflict(i, tokbuf.sp-1);
 
         }
       }
@@ -424,6 +577,15 @@ elide_trace_reduce(int yystate, int yyn)
 
   if (!show_elisions) return;
 
+  /* Check whether rule being reduced is one of type 'subscript' - to support
+     handling of BOI and VEhO */
+  if ((yyn >= LO_SUBSCRIPT) && (yyn <= HI_SUBSCRIPT)) {
+#if 0
+    printf("Setting sb[%d]=1\n", tokbuf.sp); fflush(stdout);
+#endif
+    tokbuf.flags[tokbuf.sp].subscript_before = 1;
+  }
+
   if (tokbuf.sp == tokbuf.rp) {
     /* Bison has not read the lookahead token.  Therefore, any reduction
        occurring now must be just a automatic reduction, with no option to
@@ -431,26 +593,26 @@ elide_trace_reduce(int yystate, int yyn)
        option of shifting a token further on in the chain. */
 
 #if TRACE_SR
-    printf("Reduce called in state %d, no check\n", yystate);
+    printf("Reduce called in state %d for rule %d, no check\n", yystate, yyn);
 #endif
-    return;
-  } /* else */
 
-  /* Otherwise, check which later tokens could have shifted in the state being
-     reduced.  Any that can shift will imply the need for an earlier token to
-     be present to guard against the shift happening */
+  } else {
+    /* Otherwise, check which later tokens could have shifted in the state being
+       reduced.  Any that can shift will imply the need for an earlier token to
+       be present to guard against the shift happening */
 #if TRACE_SR
-  printf("Reduce called in state %d\n", yystate);
+    printf("Reduce called in state %d for rule %d\n", yystate, yyn);
 #endif
-  for (i=tokbuf.rp; i<tokbuf.n; i++) {
-    if (can_shift_in_state(yystate, tokbuf.buf[i].value)) {
+    for (i=tokbuf.rp; i<tokbuf.n; i++) {
+      if (can_shift_in_state(yystate, tokbuf.buf[i].value)) {
 #if TRACE_SR
-        printf("Got here 3, setting [%d,%d] in state %d\n", i, tokbuf.sp, yystate);
+          printf("Got here 3, setting [%d,%d] in state %d\n", i, tokbuf.sp, yystate);
 #endif
-      set_flag(i, tokbuf.sp);
+        set_conflict(i, tokbuf.sp);
+      }
     }
   }
-
+  return;
 }
 
 /*++++++++++++++++++++++++++++++++++++++
@@ -467,6 +629,9 @@ yylex(void)
   extern YYLTYPE yylloc;
   int result;
   int old_n;
+  int is_elidable;
+  int is_boiveho;
+  int is_private;
 
   if (!show_elisions) {
     /* Minimalist approach, no need for the fancy buffering */
@@ -476,6 +641,12 @@ yylex(void)
     yylloc = tok.yylloc;
     return tok.value;
   } /* else ... */
+
+
+  /* Shift along the register to track whether we got a PA or a letter cmavo
+     right before starting a block of elidables */
+
+  prev_token_was_pa_etc = cur_token_was_pa_etc;
 
   /* Otherwise, build up a buffer of arbitrarily many elidables with a single
      non-elidable at the end.  Drain this buffer into the bison layer,
@@ -500,9 +671,15 @@ yylex(void)
       old_n = tokbuf.n;
       yylex1(&tokbuf.buf[tokbuf.n++]);
 
-    } while (is_elidable(tokbuf.buf[old_n].yylval));
+      tokbuf.flags[old_n].boiveho = 0;
+      tokbuf.flags[old_n].subscript_before = 0;
 
-    clear_flags();
+      is_elidable = token_is_elidable(tokbuf.buf[old_n].yylval, &is_boiveho);
+      tokbuf.flags[old_n].boiveho = is_boiveho;
+      tokbuf.flags[old_n].tok_is_number_marker = token_is_number_marker(tokbuf.buf[old_n].yylval);
+    } while (is_elidable);
+
+    clear_conflicts();
 
 #if 0
     fprintf(stderr, "refill gave %d tokens\n", tokbuf.n);
@@ -513,6 +690,25 @@ yylex(void)
     result = tokbuf.buf[0].value;
     tokbuf.sp = tokbuf.rp;
     ++tokbuf.rp;
+
+    if (tokbuf.rp == tokbuf.n) {
+      /* Single token, or non-elidable at end of tokbuf */
+      switch (result) {
+        case PA:
+        case BU:
+        case BY:
+        case LAU:
+        case TEI:
+        case FOI:
+          cur_token_was_pa_etc = 1;
+          break;
+        default:
+          cur_token_was_pa_etc = 0;
+          break;
+
+      }
+    }
+    
     return result;
 
   } 
