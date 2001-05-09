daab0811a1547a979fcb772f76e55f55fe152bed
richard <richard>
2001-05-09 22:06:19 +0000

    Integrate checking for lujvo containing cultural rafsi into a single state machine with the non-cultural lexer.

--- morf.c
+++ morf.c
@@ -20,7 +20,7 @@
 #include <stdio.h>
 #include <string.h>
 static int verbose=0;
-static int allow_cultural_rafsi = 0;
+static int allow_cultural_rafsi = 1; /* In testbench mode, always allow */
 #else
 
 /* When linked into the main program, get this option from the command line
@@ -31,35 +31,32 @@ extern int allow_cultural_rafsi;
 
 #include "morf.h"
 
-#define R_UNKNOWN       0
-#define R_CMAVOS        1
-#define R_CMAVOS_END_CY 2
-#define R_GISMU_0       3
-#define R_GISMU_1       4
-#define R_LUJVO_0       5
-#define R_LUJVO_1       6
-#define R_STAGE3_0      7
-#define R_STAGE3_1      8
-#define R_STAGE3_1_CVC  9
-#define R_STAGE4_0     10
-#define R_STAGE4_1     11
-#define R_CMENE        12
-#define R_BAD_TOSMABRU 13
-#define R_BAD_SLINKUI  14
+enum raw_category {
+  R_UNKNOWN,
+  R_CMAVOS, R_CMAVOS_END_CY,
+  R_GISMU_0, R_GISMU_1,
+  R_LUJVO_0, R_LUJVO_1,
+  R_CULTURAL_LUJVO_0, R_CULTURAL_LUJVO_1,
+  R_STAGE3_0, R_STAGE3_1, R_STAGE3_1_CVC,
+  R_STAGE4_0, R_STAGE4_1,
+  R_CMENE,
+  R_BAD_TOSMABRU, R_CULTURAL_BAD_TOSMABRU,
+  R_BAD_SLINKUI
+};
 
-/* Define the values returned by priority coding the bit patterns */
-#define W_UNKNOWN         0
-#define W_CMAVOS          1
-#define W_CMAVOS_END_CY   2
-#define W_GISMU           3
-#define W_LUJVO           4
-#define W_FUIVLA3         5
-#define W_FUIVLA3_CVC     6
-#define W_FUIVLA4         7
-#define W_CMENE           8
-#define W_BAD_TOSMABRU    9
-#define W_BAD_SLINKUI    10
-#define W_BIZARRE        11
+enum processed_category {
+  W_UNKNOWN,
+  W_CMAVOS, W_CMAVOS_END_CY,
+  W_GISMU,
+  W_LUJVO,
+  W_CULTURAL_LUJVO,
+  W_FUIVLA3, W_FUIVLA3_CVC,
+  W_FUIVLA4,
+  W_CMENE,
+  W_BAD_TOSMABRU, W_CULTURAL_BAD_TOSMABRU,
+  W_BAD_SLINKUI,
+  W_BIZARRE
+};
 
 /* Include table for turning the letter stream into meta-classes (consonant,
  * vowel, permissible pair etc).  These 'meta-classes' are the tokens used by
@@ -72,10 +69,6 @@ extern int allow_cultural_rafsi;
 /* Include the main DFA scanning tables (including cultural rafsi) built by
    nfa2dfa.pl. */
 #include "morf_dfa.c"
-
-/* Include the main DFA scanning tables (excluding cultural rafsi) built by
-   nfa2dfa.pl. */
-#include "morfnc_dfa.c"
 
 /* Map N->1, R->2, other C->3, else ->0.  Used to trim down the last-but-one
  * letter, which is saved to allow the front-end to spot illegal triples and
@@ -205,32 +198,21 @@ static unsigned char vmapchar[256] = {
   0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01
 };
 
-
-struct StateTransTable {
-  short *ns;
-  unsigned char *tok;
-  unsigned short *base;
-  short *def;
-};
-
-static struct StateTransTable morf_stt = {morf_nextstate, morf_token, morf_base, morf_defstate};
-static struct StateTransTable morfnc_stt = {morfnc_nextstate, morfnc_token, morfnc_base, morfnc_defstate};
-
 /*********************************************************************/
 /* Given the current state and the 'token' read, find the next state */
 /*********************************************************************/
 
-int find_next_state(struct StateTransTable *tab, int cs, unsigned char k)
+int find_next_state(int cs, unsigned char k)
 {
   int h, l, m;
   unsigned char xm;
-  unsigned char *t = tab->tok;
-  unsigned short *nstab = tab->ns;
+  unsigned char *t = morf_token;
+  unsigned short *nstab = morf_nextstate;
   unsigned int kk = k;
 
   while (cs >= 0) {
-    l = tab->base[cs];
-    h = tab->base[cs+1];
+    l = morf_base[cs];
+    h = morf_base[cs+1];
     while (h > l) {
       m = (h + l) >> 1;
       xm = t[m];
@@ -239,7 +221,7 @@ int find_next_state(struct StateTransTable *tab, int cs, unsigned char k)
       if (xm>kk) h = m;
       else       l = m;
     }
-    cs = tab->def[cs]; /* Move onto next subtable to check. */
+    cs = morf_defstate[cs]; /* Move onto next subtable to check. */
   }
 
   /* Tried all tables without a match, fail */
@@ -264,24 +246,20 @@ morf_scan(char *s, char ***buf_end)
   int ent, tent, tok, set, inhibit;
   int state, next_state;
   int inhibited = 0, initial = 1;
-  int exival;
   int decrement = 0;
-  int result;
+  enum raw_category exival;
+  enum processed_category result;
   int had_uppercase=0;
   int letter_uppercase;
   int ended_with_comma=0;
   int started_with_comma=0;
   MorfType ext_result;
-  short *exitval_table;
-  struct StateTransTable *stt;
 
   typedef enum {
     ACT_CLEAR=0, ACT_SHIFT=1, ACT_FREEZE=2
   } Action;
 
   Action act, last_act;
-  
-  stt = allow_cultural_rafsi ? &morf_stt : &morfnc_stt;
   
   start = *buf_end;
   pstart = start;
@@ -366,7 +344,7 @@ morf_scan(char *s, char ***buf_end)
     p++;
     initial = 0;
 
-    next_state = find_next_state(stt, state, tok);
+    next_state = find_next_state(state, tok);
 
 #ifdef TEST_MORF
     if (verbose) {
@@ -403,14 +381,12 @@ morf_scan(char *s, char ***buf_end)
     ended_with_comma = 1;
   }
   
-  exitval_table = allow_cultural_rafsi ? morf_exitval : morfnc_exitval;
-  
-  if ((state < 0) || started_with_comma || ended_with_comma || (exitval_table[state] == 0)) {
+  if ((state < 0) || started_with_comma || ended_with_comma || (morf_exitval[state] == R_UNKNOWN)) {
     result = W_UNKNOWN;
     ext_result = MT_BOGUS;
     decrement = 0;
   } else {
-    exival = exitval_table[state];
+    exival = morf_exitval[state];
     switch (exival) {
       case R_CMAVOS: result = W_CMAVOS; decrement = 0; break;
       case R_CMAVOS_END_CY: result = W_CMAVOS_END_CY; decrement = 0; break;
@@ -418,6 +394,8 @@ morf_scan(char *s, char ***buf_end)
       case R_GISMU_1: result = W_GISMU; decrement = 1; break;
       case R_LUJVO_0: result = W_LUJVO; decrement = 0; break;
       case R_LUJVO_1: result = W_LUJVO; decrement = 1; break;
+      case R_CULTURAL_LUJVO_0: result = W_CULTURAL_LUJVO; decrement = 0; break;
+      case R_CULTURAL_LUJVO_1: result = W_CULTURAL_LUJVO; decrement = 1; break;
       case R_STAGE3_0: result = W_FUIVLA3; decrement = 0; break;
       case R_STAGE3_1: result = W_FUIVLA3; decrement = 1; break;
       case R_STAGE3_1_CVC: result = W_FUIVLA3_CVC; decrement = 1; break;
@@ -425,6 +403,7 @@ morf_scan(char *s, char ***buf_end)
       case R_STAGE4_1: result = W_FUIVLA4; decrement = 1; break;
       case R_CMENE: result = W_CMENE; decrement = 0; break;
       case R_BAD_TOSMABRU: result = W_BAD_TOSMABRU; decrement = 1; break;
+      case R_CULTURAL_BAD_TOSMABRU: result = W_CULTURAL_BAD_TOSMABRU; decrement = 1; break;
       case R_BAD_SLINKUI: result = W_BAD_SLINKUI; decrement = 0; break;
       case R_UNKNOWN:
       default:
@@ -453,6 +432,11 @@ morf_scan(char *s, char ***buf_end)
         ext_result = had_uppercase ? MT_BAD_UPPERCASE : MT_LUJVO;
         if (decrement) pstart--;
         break;
+      case W_CULTURAL_LUJVO:
+        ext_result = had_uppercase        ? MT_BAD_UPPERCASE :
+                     allow_cultural_rafsi ? MT_LUJVO : MT_BOGUS ;
+        if (decrement) pstart--;
+        break;
       case W_FUIVLA3:
         ext_result = had_uppercase ? MT_BAD_UPPERCASE : MT_FUIVLA3;
         if (decrement) pstart--;
@@ -466,6 +450,7 @@ morf_scan(char *s, char ***buf_end)
         if (decrement) pstart--;
         break;
       case W_BAD_TOSMABRU:
+      case W_CULTURAL_BAD_TOSMABRU:
       case W_BAD_SLINKUI:
         /* Don't care about uppercase/lowercase status */
         ext_result = MT_BOGUS;
@@ -483,7 +468,7 @@ morf_scan(char *s, char ***buf_end)
   }
 
 #ifdef TEST_MORF
-  if ((state < 0) || (exitval_table[state] == 0)) {
+  if ((state < 0) || (morf_exitval[state] == 0)) {
     printf("%-25s : UNMATCHED!\n", s);
   } else {
     char *a;
@@ -509,6 +494,9 @@ morf_scan(char *s, char ***buf_end)
       case W_LUJVO:
         printf("lujvo");
         break;
+      case W_CULTURAL_LUJVO:
+        printf("lujvo (with cultural rafsi)");
+        break;
       case W_FUIVLA3_CVC:
         printf("fu'ivla (stage-3 short rafsi)");
         break;
@@ -524,6 +512,9 @@ morf_scan(char *s, char ***buf_end)
       case W_BAD_TOSMABRU:
         printf("Bad lujvo (y hyphen not required)");
         break;
+      case W_CULTURAL_BAD_TOSMABRU:
+        printf("Bad lujvo (y hyphen not required, with cultural rafsi)");
+        break;
       case W_BAD_SLINKUI:
         printf("Bad fu'ivla (fails slinku'i test)");
         break;
@@ -538,14 +529,20 @@ morf_scan(char *s, char ***buf_end)
       case W_CMAVOS_END_CY:
       case W_GISMU:
       case W_LUJVO:
+      case W_CULTURAL_LUJVO:
       case W_FUIVLA3:
       case W_FUIVLA3_CVC:
       case W_FUIVLA4:
       case W_BAD_TOSMABRU:
+      case W_CULTURAL_BAD_TOSMABRU:
       case W_BAD_SLINKUI:
         if (had_uppercase) {
           printf(" (contains invalid uppercase)");
         }
+        break;
+      case W_CMENE:
+      case W_BIZARRE:
+        /* Nothing to do */
         break;
     }
 
@@ -578,8 +575,6 @@ int main (int argc, char **argv) {
   while (++argv, --argc) {
     if (!strncmp(*argv, "-v", 2)) {
       verbose = 1;
-    } else if (!strncmp(*argv, "-cr", 2)) {
-      allow_cultural_rafsi = 1;
     } else if (!strncmp(*argv, "-", 1)) {
       fprintf(stderr, "Unrecognized command line argument '%s'\n", *argv);
     } else {
