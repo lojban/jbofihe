c9e9677ad78e651f4aefb4b9fd9a006f50912c80
richard <richard>
2001-07-08 21:52:37 +0000

    Completely new is_valid_lujvo function (previous one was beyond repair.) Add testbench for the is_valid_lujvo function.  New pattern testing functions : is_bad_triple, is_cvccy, is_ccvcy, is_cvav.  Add folding.  Fix compilation warnings.

--- lujvofns.c
+++ lujvofns.c
@@ -20,7 +20,7 @@
 #define UNVOICED  0100
 #define UPPERCASE 0200
 
-unsigned char attr_table[256] = {
+unsigned char attr_table[256] = {/*{{{*/
   0000, 0000, 0000, 0000, 0000, 0000, 0000, 0000, /* 000 - 007 */
   0000, 0000, 0000, 0000, 0000, 0000, 0000, 0000, /* 010 - 017 */
   0000, 0000, 0000, 0000, 0000, 0000, 0000, 0000, /* 020 - 027 */
@@ -56,98 +56,36 @@ unsigned char attr_table[256] = {
   0000, 0000, 0000, 0000, 0000, 0000, 0000, 0000, /* 350 - 357 */
   0000, 0000, 0000, 0000, 0000, 0000, 0000, 0000, /* 360 - 367 */
   0000, 0000, 0000, 0000, 0000, 0000, 0000, 0000  /* 370 - 377 */
-};
+};/*}}}*/
 
-/*++++++++++++++++++++++++++++++++++++++
-  
-
-  int is_consonant
-
-  char c
-  ++++++++++++++++++++++++++++++++++++++*/
-
-int
-is_consonant(char c) {
-  return !!(attr_table[c] & CONSONANT);
+int is_consonant(char c) {/*{{{*/
+  return !!(attr_table[(unsigned)c & 0xff] & CONSONANT);
 }
-
-/*++++++++++++++++++++++++++++++++++++++
-  Check whether a character is a consonant.
-
-  int is_consonant
-
-  char c
-  ++++++++++++++++++++++++++++++++++++++*/
-
-int
-is_uppercase_consonant(char c) {
+/*}}}*/
+int is_uppercase_consonant(char c) {/*{{{*/
   int m = CONSONANT | UPPERCASE;
-  return ((attr_table[c] & m) == m);
+  return ((attr_table[(unsigned)c & 0xff] & m) == m);
 }
-
-/*++++++++++++++++++++++++++++++++++++++
-  
-
-  int is_vowel
-
-  char c
-  ++++++++++++++++++++++++++++++++++++++*/
-
-int
-is_vowel(char c) {
-  return !!(attr_table[c] & VOWEL);
+/*}}}*/
+int is_vowel(char c) {/*{{{*/
+  return !!(attr_table[(unsigned)c & 0xff] & VOWEL);
 }
-
-/*++++++++++++++++++++++++++++++++++++++
-  
-
-  int is_voiced
-
-  char c
-  ++++++++++++++++++++++++++++++++++++++*/
-
-int
-is_voiced(char c) {
-  return !!(attr_table[c] & VOICED);
+/*}}}*/
+int is_voiced(char c) {/*{{{*/
+  return !!(attr_table[(unsigned)c & 0xff] & VOICED);
 }
-
-/*++++++++++++++++++++++++++++++++++++++
-  
-
-  int is_unvoiced
-
-  char c
-  ++++++++++++++++++++++++++++++++++++++*/
-
-int
-is_unvoiced(char c) {
-  return !!(attr_table[c] & UNVOICED);
+/*}}}*/
+int is_unvoiced(char c) {/*{{{*/
+  return !!(attr_table[(unsigned)c & 0xff] & UNVOICED);
 }
-
-/*++++++++++++++++++++++++++++++++++++++
-  
-
-  int is_sibilant
-
-  char c
-  ++++++++++++++++++++++++++++++++++++++*/
-
-int
-is_sibilant(char c) {
-  return !!(attr_table[c] & SIBILANT);
+/*}}}*/
+int is_sibilant(char c) {/*{{{*/
+  return !!(attr_table[(unsigned)c & 0xff] & SIBILANT);
 }
-
-/*++++++++++++++++++++++++++++++++++++++
-  
-
-  int is_ccv
-
-  char *s
-  ++++++++++++++++++++++++++++++++++++++*/
-
-int
-is_ccv(char *s) {
-  if (is_consonant(s[0]) &&
+/*}}}*/
+int is_ccv(char *s) {/*{{{*/
+  if ((strlen(s) >= 3) &&
+      is_consonant(s[0]) &&
       is_consonant(s[1]) &&
       is_vowel(s[2])) {
     return 1;
@@ -155,40 +93,35 @@ is_ccv(char *s) {
     return 0;
   }
 }
-
-
-/*++++++++++++++++++++++++++++++++++++++
-  
-
-  int is_cvv
-
-  char *s
-  ++++++++++++++++++++++++++++++++++++++*/
-
-int
-is_cvv(char *s) {
-  if (is_consonant(s[0]) &&
+/*}}}*/
+int is_cvv(char *s) {/*{{{*/
+  int len = strlen(s);
+  if ((len >= 3) && 
+      is_consonant(s[0]) &&
       is_vowel(s[1]) &&
-      (is_vowel(s[2]) ||
-       (is_vowel(s[3]) && s[2] == '\''))) {
+      is_vowel(s[2])) {
     return 1;
   } else {
     return 0;
   }
 }
-
-
-/*++++++++++++++++++++++++++++++++++++++
-  
-
-  int is_cvc
-
-  char *s
-  ++++++++++++++++++++++++++++++++++++++*/
-
-int
-is_cvc(char *s) {
-  if (is_consonant(s[0]) &&
+/*}}}*/
+int is_cvav(char *s) {/*{{{*/
+  int len = strlen(s);
+  if ((len >= 4) && 
+      is_consonant(s[0]) &&
+      is_vowel(s[1]) &&
+      (s[2] == '\'') &&
+      is_vowel(s[3])) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+/*}}}*/
+int is_cvc(char *s) {/*{{{*/
+  if ((strlen(s) >= 3) &&
+      is_consonant(s[0]) &&
       is_vowel(s[1]) &&
       is_consonant(s[2])) {
     return 1;
@@ -196,19 +129,10 @@ is_cvc(char *s) {
     return 0;
   }
 }
-
-
-/*++++++++++++++++++++++++++++++++++++++
-  
-
-  int is_cvccv
-
-  char *s
-  ++++++++++++++++++++++++++++++++++++++*/
-
-int
-is_cvccv(char *s) {
-  if (is_consonant(s[0]) &&
+/*}}}*/
+int is_cvccv(char *s) {/*{{{*/
+  if ((strlen(s) >= 5) &&
+      is_consonant(s[0]) &&
       is_vowel(s[1]) &&
       is_consonant(s[2]) &&
       is_consonant(s[3]) &&
@@ -217,20 +141,22 @@ is_cvccv(char *s) {
   } else {
     return 0;
   }
-}
-
-
-/*++++++++++++++++++++++++++++++++++++++
-  
-
-  int is_ccvcv
-
-  char *s
-  ++++++++++++++++++++++++++++++++++++++*/
-
-int
-is_ccvcv(char *s) {
-  if (is_consonant(s[0]) &&
+}/*}}}*/
+int is_cvccy(char *s) {/*{{{*/
+  if ((strlen(s) >= 5) &&
+      is_consonant(s[0]) &&
+      is_vowel(s[1]) &&
+      is_consonant(s[2]) &&
+      is_consonant(s[3]) &&
+      (s[4] == 'y')) {
+    return 1;
+  } else {
+    return 0;
+  }
+}/*}}}*/
+int is_ccvcv(char *s) {/*{{{*/
+  if ((strlen(s) >= 5) &&
+      is_consonant(s[0]) &&
       is_consonant(s[1]) &&
       is_vowel(s[2]) &&
       is_consonant(s[3]) &&
@@ -239,18 +165,21 @@ is_ccvcv(char *s) {
   } else {
     return 0;
   }
-}
-
-/*++++++++++++++++++++++++++++++++++++++
-  Check whether a consonant pair is permissible
-
-  int is_pairok
-
-  char *s
-  ++++++++++++++++++++++++++++++++++++++*/
-
-int
-is_pairok(char *s) {
+}/*}}}*/
+int is_ccvcy(char *s) {/*{{{*/
+  if ((strlen(s) >= 5) &&
+      is_consonant(s[0]) &&
+      is_consonant(s[1]) &&
+      is_vowel(s[2]) &&
+      is_consonant(s[3]) &&
+      (s[4] == 'y')) {
+    return 1;
+  } else {
+    return 0;
+  }
+}/*}}}*/
+int is_pairok(char *s) {/*{{{*/
+  /* Check whether a consonant pair is permissible */
   int test1, test2, test3, test4; /* all true if acceptable */
 
   test1 = (s[0] != s[1]);
@@ -263,17 +192,10 @@ is_pairok(char *s) {
   return test1 && test2 && test3 && test4;
 
 }
-
-/*++++++++++++++++++++++++++++++++++++++
-  Return 1 if an initial consonant pair is acceptable for a lujvo, otherwise return 0.
-
-  int is_initial_pair_ok
-
-  char *s The string whose first 2 chars are to be tested.
-  ++++++++++++++++++++++++++++++++++++++*/
-
-int
-is_initialpairok(char *s) {
+/*}}}*/
+int is_initialpairok(char *s) {/*{{{*/
+  /* Return 1 if an initial consonant pair is acceptable for a lujvo, otherwise
+     return 0. */
 
   switch (s[0]) {
     case 'b':
@@ -299,112 +221,153 @@ is_initialpairok(char *s) {
       return 0;
   }
 }
+/*}}}*/
+int is_bad_triple (char *s) {/*{{{*/
+/* Check whether a triple is bad */
+  if (!strcmp(s,"ntc") || !strcmp(s,"nts") || !strcmp(s,"ndj") || !strcmp(s,"ndz"))
+    return 1;
+  else
+    return 0;
+}
+/*}}}*/
 
+/* ========================================*/
 
-/*++++++++++++++++++++++++++++++++++++++
-  Return 1 if the string t has the correct form to be a lujvo (note,
-  there is no dictionary lookup of the apparent constituent rafsi)
+#define ADVANCE(n) t+=(n), len-=(n)
 
-  int is_valid_lujvo
-
-  char *t
-  ++++++++++++++++++++++++++++++++++++++*/
+/* is_valid_lujvo(), used by the jvocu'adju program to do tosmabru checks. */
 
 int
-is_valid_lujvo(char *t) {
-  char *p;
-  int debug = 0;
+is_valid_lujvo(char *t)
+{
+  int len = strlen(t);
+  char buf[1024];
 
-  if (debug) printf("Testing [%s] as valid lujvo\n", t);
-  if (strlen(t) < 5) {
-    if (debug) printf("Initial length too short, invalid\n");
-    return 0;
-  }
-  while (*t) {
-    if (debug) printf("Residual [%s]\n", t);
-    p = strchr(t, 'y');
-    if ((p-t) == 4) {
-      /* Starts with abcdy, have to check whether abcd could be a valid rafsi */
-      if (is_consonant(t[0]) && is_consonant(t[1]) && is_vowel(t[2]) && is_consonant(t[3]) && is_initialpairok(t)) {
-        if (debug) printf("4 letter rafsi CCVC at start valid then y\n");
-        t+=5;
-      } else if (is_consonant(t[0]) && is_vowel(t[1]) && is_consonant(t[2]) && is_consonant(t[3]) && is_pairok(t+2)) {
-        if (debug) printf("4 letter rafsi CVCC at start valid then y\n");
-        t+=5;
+  /* Flags to check CVV + hyphen stuff. */
+  int start_cvv;
+  int had_rn_hyphen;
+  int final_ccv;
+
+  /* Number of components seen */
+  int nrafsi;
+  
+  start_cvv = 0;
+  had_rn_hyphen = 0;
+  nrafsi = 0;
+
+  for (;;) {
+    if ((len == 5) && is_cvccv(t)) {/*{{{*/
+      if (nrafsi==0) return 0; /* Can't be initial */
+      if (!is_pairok(t+2)) return 0;
+      ADVANCE(5);
+      /*}}}*/
+    } else if ((len == 5) && is_ccvcv(t)) {/*{{{*/
+      if (nrafsi==0) return 0; /* Can't be initial */
+      if (!is_pairok(t)) return 0;
+      ADVANCE(5);
+      /*}}}*/
+    } else if (is_cvccy(t)) {/*{{{*/
+      if (len==5) return 0; /* Can't be final */
+      if (!is_pairok(t+2)) return 0;
+      ADVANCE(5);
+      /*}}}*/
+    } else if (is_ccvcy(t)) {/*{{{*/
+      if (len==5) return 0; /* Can't be final */
+      if (!is_initialpairok(t)) return 0;
+      ADVANCE(5);
+/*}}}*/
+    } else if (is_cvc(t)) {/*{{{*/
+      int pair_ok, bad_triple, smabru, initial, need_y;
+      if (len<6) return 0; /* Can't be final, must be at least 3 letters after
+                              now */
+      if (t[3] == 'y') {
+        buf[0] = t[2];
+        strcpy(buf+1, t+4);
+        pair_ok = is_pairok(buf);
+        bad_triple = is_bad_triple(buf);
+        initial = (nrafsi == 0);
+        smabru = initial ? is_valid_lujvo(buf) : 0;
+        need_y = bad_triple || (initial && smabru);
+
+        /* Check whether the y is unnecessary */
+        if (pair_ok && !need_y) return 0;
+            
+        ADVANCE(4);
+
       } else {
-        if (debug) printf("4 letter rafsi at start invalid\n");
-        return 0;
+        
+        pair_ok = is_pairok(t+2);
+        bad_triple = is_bad_triple(t+2);
+        initial = (nrafsi == 0);
+        smabru = initial ? is_valid_lujvo(t+2) : 0;
+        need_y = bad_triple || (initial && smabru);
+
+        /* Check whether there should be a y */
+        if (!pair_ok || need_y) return 0;
+            
+        ADVANCE(3);
       }
-    } else if ((p-t) == 3) {
-      /* Starts with 3 letter rafsi then a join */
-      if (is_ccv(t)) {
-        if (is_initialpairok(t)) {
-          /* Perhaps ought to check whether rafsi form is in dictionary? */
-          if (debug) printf("3 letter rafsi CCV at start valid then y\n");
-          return 1;
+      /*}}}*/
+    } else if (is_cvv(t)) {/*{{{*/
+      if (nrafsi == 0) {
+        if (len < 6) return 0;
+        /* Strip hyphen */
+        start_cvv = 1;
+        if (((t[3] == 'r') && (is_consonant(t[4]) && (t[4] != 'r'))) ||
+            ((t[3] == 'n') && (t[4] == 'r'))) {
+          had_rn_hyphen = 1;         
+          ADVANCE(4);
         } else {
-          if (debug) printf("3 letter rafsi CCV at start invalid then y\n");
-          return 0;
+          ADVANCE(3);
         }
-      } else if (is_cvc(t)) {
-        /* Dictionary test? */
-        if (debug) printf("3 letter rafsi CVC at start valid then y\n");
-        return 1;
-      } else if (is_cvv(t)) {
-        /* Not possible */
-        fprintf(stderr, "Can't have y after CVV form rafsi\n");
-        exit(1);
+      } else {
+        ADVANCE(3);
       }
-    } else if ((p-t) < 3) {
-      if (debug) printf("<3 letters left, invalid\n");
+      /*}}}*/
+    } else if (is_cvav(t)) {/*{{{*/
+      if (nrafsi == 0) {
+        if (len < 7) return 0;
+        /* Strip hyphen */
+        start_cvv = 1;
+        if (((t[4] == 'r') && (is_consonant(t[5]) && (t[5] != 'r'))) ||
+            ((t[4] == 'n') && (t[5] == 'r'))) {
+          had_rn_hyphen = 1;         
+          ADVANCE(5);
+        } else {
+          ADVANCE(4);
+        }
+      } else {
+        ADVANCE(4);
+      }
+      /*}}}*/
+    } else if (is_ccv(t)) {/*{{{*/
+      if (!is_initialpairok(t)) return 0;
+ 
+      if (len == 3) final_ccv = 1;
+      ADVANCE(3);
+/*}}}*/
+    } else {/*{{{*/
+      /* Anything else is invalid */
       return 0;
-    } else if ((strlen(t) > 5) || (strlen(t) == 3)) {
-      /* Strip leading rafsi if valid */
-      if (is_ccv(t)) {
-        if (is_initialpairok(t)) {
-          if (debug) printf("Initial CCV, examine tail\n");
-          t+=3;
-          /* Go round loop again */
-        } else {
-          if (debug) printf("Initial invalid CCV\n");
-          return 0;
-        }
-      } else if (is_cvc(t)) {
-        if (debug) printf("Initial CVC, examine tail\n");
-        t+=3;
-        /* Go round again */
-      } else if (is_cvv(t)) {
-        if (debug) printf("Initial CVV, examine tail\n");
-        if (t[2] == '\'') {
-          t+=4;
-        } else {
-          t+=3;
-        }
-      } else {
-        /* Not valid lujvo */
-        if (debug) printf("Invalid, prefix not any rafsi form\n");
-        return 0;
-      }
-    } else if (strlen(t) == 5) {
-      /* Just a gismu left, assume OK if correct form */
-      if (is_cvccv(t) || is_ccvcv(t)) { 
-        if (debug) printf("Matches gismu form\n");
-        return 1;
-      } else {
-        if (debug) printf("Unmatched 5 character form\n");
-        return 0;
-      }
-    } else {
-      if (debug) printf("Unrecognized length\n");
-      return 0;
+/*}}}*/
     }
+
+    nrafsi++;
+    if (!*t) break; /* At end of word */
   }
-  /* If we fall out of the loop, all rafsi have been checked off. */
-  return 1;
+
+  /* Final processing to check validity */
+  if (start_cvv) {
+    if (((nrafsi == 2) && final_ccv) || had_rn_hyphen)
+      return 1;
+    else
+      return 0;
+  } else {
+    return 1;
+  }
 }
 
-
-#ifdef TEST
+#ifdef TEST_PRIMITIVES
 int main() {
   int c;
   for (c='a'; c<='z'; c++) {
@@ -413,5 +376,23 @@ int main() {
   }
   return 0;
 }
+#endif
+
+#ifdef TEST_IS_VALID_LUJVO
+int main(int argc, char **argv) {
+  int ivl;
+  
+  if (argc < 2) {
+    fprintf(stderr, "Need a lujvo to check as an argument\n");
+    exit(1);
+  }
+
+  ivl = is_valid_lujvo(argv[1]);
+
+  printf("%s : %s\n", argv[1], ivl ? "is a lujvo" : "not a lujvo");
+
+  return !ivl;
+}
+
 #endif
 
